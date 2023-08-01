6b329300d13a563d462784d07d438c44c2b27486
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-01 18:56:12 -0400

    Menu e inicio das funções

--- src/main/java/org/example/Function.java
+++ src/main/java/org/example/Function.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Function {
     static Scanner scanner = new Scanner(System.in);
 
+    public static int menu(List<Cliente> clienteList, List<Pedido> pedidoList){
+        int option;
+        System.out.println("\t\tSISTEMA DE VENDAS");
+        System.out.println("1 - Clientes");
+        System.out.println("2 - Pedidos");
+        System.out.println("3 - Sair");
+        System.out.print("\nSelecione a opção desejada: ");
+        option = scanner.nextInt();
 
+        switch (option){
+            case 1 -> menuCliente(clienteList);
+            case 2 -> menuPedido(clienteList, pedidoList);
+        }
+
+        return option;
+    }
+
+    public static void menuCliente(List<Cliente> clienteList){
+
+    }
+    public static void menuPedido(List<Cliente> clienteList, List<Pedido> pedidoList){
+
+    }
+
+    public static void buscarPedidos(List<Pedido> pedidoList){
+
+    }
 }
